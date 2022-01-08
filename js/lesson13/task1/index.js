af5dc9d0bfb52c97890f1f73838e52002e6e7241
anastasiiashev888 <you@example.com>
2022-01-08 02:08:19 +0200

    1

--- js/lesson13/task1/index.js
+++
@@ -1,1 +0,0 @@
-export default (arr) => arr.filter((num) => num % 2 === 0);
--- js/lesson13/task1/index.test.js
+++ js/lesson13/task1/index.test.js
@@ -1,14 +1,14 @@
-import getEvenNumbers from './index';
-
-it('17 is equal to 17', () => {
-  expect(17).toBe(17);
+it('17 expect 17', () => {
+  expect(17).toEqual(17);
 });
 
-it('18 is not equal to 17', () => {
-  expect(18).not.toBe(17);
+it('17 it not 1', () => {
+  expect(17).not.toEqual(18);
 });
 
-it('get even numbers', () => {
-  const result = getEvenNumbers([1, 2, 3, 4, 5, 6, 7]);
-  expect(result).toEqual([2, 4, 6]);
+const getEvenNumbers = (numbers) => numbers.filter((num) => num % 2 === 0);
+
+it('should get only even number', () => {
+  const result = getEvenNumbers([1, 2, 3, 4]);
+  expect(result).toEqual([2, 4]);
 });
