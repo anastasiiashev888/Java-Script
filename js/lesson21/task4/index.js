1865e3a61a81223a58d132c8a967a182689576cc
anastasiiashev888 <you@example.com>
2022-02-04 01:19:40 +0200

    1

--- js/lesson21/task4/index.js
+++ js/lesson21/task4/index.js
@@ -1,14 +1,14 @@
 export const getTitle = () => {
-  return document.querySelector('title').textContent;
+  return document.querySelector('.title').textContent;
 };
 export const getDescription = () => {
-  return document.querySelector('about').innerText;
+  return document.querySelector('.about').innerText;
 };
 export const getPlans = () => {
-  return document.querySelector('plans').innerHTML;
+  return document.querySelector('.plans').innerHTML;
 };
 export const getGoal = () => {
-  return document.querySelector('goal').outerHTML;
+  return document.querySelector('.goal').outerHTML;
 };
 getTitle();
 getDescription();
