0632990373bfa46a507d039cdd7684b548fb3190
anastasiiashev888 <you@example.com>
2022-02-03 23:57:09 +0200

    1

---
+++ js/lesson21/task3/index.js
@@ -0,0 +1,13 @@
+const getItemsList = () => {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(Array.from(elementsList));
+  return Array.from(elementsList);
+};
+
+const getItemsArray = () => {
+  const elementsArray = document.querySelectorAll('.tool');
+  console.dir(Array.from(elementsArray));
+  return Array.from(elementsArray);
+};
+getItemsArray();
+getItemsList();
